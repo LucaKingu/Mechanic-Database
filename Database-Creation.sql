3a8cf77e2b6e638ccdcce4ec1226bb7457c1cbd8
Luca Alfino <alfinoluca21@gmail.com>
2024-02-01 15:31:52 +0100

    Added customer and altered mechanic

--- Database-Creation.sql
+++ Database-Creation.sql
@@ -1,6 +1,19 @@
 --Since I created DB manually, uncomment below line to create DB via script
 --CREATE DATABASE MechanicDB
 
+
+CREATE TABLE customer
+(
+	customerId INT PRIMARY KEY IDENTITY(1,1),
+	firstName NVARCHAR(25) NOT NULL,
+	lastName NVARCHAR(25) NOT NULL,
+	contactNumber VARCHAR(25) NOT NULL,
+	email NVARCHAR(40) NOT NULL,
+	address NVARCHAR(80) NOT NULL,
+	mechanicId INT NOT NULL CONSTRAINT cs_mId_fk REFERENCES mechanic(mechanicId)
+);
+
+
 CREATE TABLE mechanic
 (
 	mechanicId INT PRIMARY KEY IDENTITY(1,1),	--Auto incrementing
@@ -10,6 +23,7 @@ CREATE TABLE mechanic
 	contactNumber VARCHAR(25) NOT NULL, --Catering for international long numbers
 	email NVARCHAR(40) NOT NULL
 );
+--One customer can have one mechanic
 
 CREATE TABLE [service]
 (
@@ -19,3 +33,9 @@ CREATE TABLE [service]
 	[status] BIT NOT NULL,	--Takes 1 if the car is serviced and requires no further intervention, takes -1 if car is required/expected to come back to be serviced.
 	mechanicId INT NOT NULL CONSTRAINT se_mId_fk REFERENCES mechanic(mechanicId)
 );
+--One mechanic can perform one service
+
+
+
+
+
