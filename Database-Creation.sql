752e27e8915b213063f526fb6e81884fe61f1b78
Luca Alfino <alfinoluca21@gmail.com>
2024-02-01 14:42:20 +0100

    Added mechanic table

--- Database-Creation.sql
+++ Database-Creation.sql
@@ -1,7 +1,12 @@
---Since I created DB manually, uncomment below line to create via script
+--Since I created DB manually, uncomment below line to create DB via script
 --CREATE DATABASE MechanicDB
 
-create table test
+CREATE TABLE mechanic
 (
-	testId int primary key
+	mechanicId int primary key IDENTITY(1,1),	--Auto incrementing
+	firstName NVARCHAR(25) NOT NULL,
+	lastName NVARCHAR(25) NOT NULL,
+	salary SMALLMONEY NOT NULL,
+	contactNumber varchar(25) NOT NULL, --Catering for international long numbers
+	email NVARCHAR(40) NOT NULL
 );
