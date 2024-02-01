1225d1113f00a4905d364eb91ef0dbabab958a6e
Luca Alfino <alfinoluca21@gmail.com>
2024-02-01 14:55:06 +0100

    Added service table

--- Database-Creation.sql
+++ Database-Creation.sql
@@ -3,10 +3,19 @@
 
 CREATE TABLE mechanic
 (
-	mechanicId int primary key IDENTITY(1,1),	--Auto incrementing
+	mechanicId INT PRIMARY KEY IDENTITY(1,1),	--Auto incrementing
 	firstName NVARCHAR(25) NOT NULL,
 	lastName NVARCHAR(25) NOT NULL,
 	salary SMALLMONEY NOT NULL,
-	contactNumber varchar(25) NOT NULL, --Catering for international long numbers
+	contactNumber VARCHAR(25) NOT NULL, --Catering for international long numbers
 	email NVARCHAR(40) NOT NULL
+);
+
+CREATE TABLE [service]
+(
+	serviceId INT PRIMARY KEY IDENTITY(1,1),
+	serviceDate DATE NOT NULL,
+	[description] NVARCHAR(300),	--Descriptions are not always needed
+	[status] BIT NOT NULL,	--Takes 1 if the car is serviced and requires no further intervention, takes -1 if car is required/expected to come back to be serviced.
+	mechanicId INT NOT NULL CONSTRAINT se_mId_fk REFERENCES mechanic(mechanicId)
 );
