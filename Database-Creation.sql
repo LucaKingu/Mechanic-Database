50e55f1de0839a2ff55fa643b13112079cf07a5c
Luca Alfino <alfinoluca21@gmail.com>
2024-02-02 14:17:57 +0100

    Modified database

--- Database-Creation.sql
+++ Database-Creation.sql
@@ -9,6 +9,40 @@ CREATE TABLE [service]
 	serviceDescription NVARCHAR(300) NOT NULL,
 );
 
+CREATE TABLE mechanic
+(
+	mechanicId INT PRIMARY KEY IDENTITY(1,1),	--Auto incrementing
+	firstName NVARCHAR(25) NOT NULL,
+	lastName NVARCHAR(25) NOT NULL,
+	salary SMALLMONEY NOT NULL,
+	contactNumber VARCHAR(25) NOT NULL, --Catering for international long numbers
+	email NVARCHAR(40) NOT NULL,
+	serviceId INT NOT NULL CONSTRAINT mh_sId_fk FOREIGN KEY(serviceId) REFERENCES [service](serviceId),
+);
+--One service could contain many mechanics
+
+CREATE TABLE customer
+(
+	customerId INT PRIMARY KEY IDENTITY(1,1),
+	firstName NVARCHAR(25) NOT NULL,
+	lastName NVARCHAR(25) NOT NULL,
+	contactNumber VARCHAR(25) NOT NULL,
+	email NVARCHAR(40) NOT NULL,
+	[address] NVARCHAR(80) NOT NULL,
+);
+
+CREATE TABLE vehicle
+(
+	vehicleId INT PRIMARY KEY IDENTITY(1,1),
+	brand NVARCHAR(30) NOT NULL,
+	model NVARCHAR(30) NOT NULL,
+	[year] DATE NOT NULL,
+	plate NVARCHAR(30) NOT NULL,	--Also catering for international and custom number plates
+	customerId INT NOT NULL CONSTRAINT vh_cId_fk FOREIGN KEY(customerId) REFERENCES customer(customerId)
+);
+--One customer could contain many vehicles
+
+
 CREATE TABLE serviceRecord
 (
 	recordId INT PRIMARY KEY IDENTITY(1,1),
@@ -19,50 +53,12 @@ CREATE TABLE serviceRecord
 	partsUsed NVARCHAR(300) NOT NULL,
 	totalCost SMALLMONEY NOT NULL,
 	serviceId INT NOT NULL CONSTRAINT sr_sId_fk FOREIGN KEY(serviceId) REFERENCES [service](serviceId),
-	vehicleID INT NOT NULL CONSTRAINT sr_vId_fk FOREIGN KEY(vehicleId) REFERENCES vehicle(vehicleId)
+	customerId INT NOT NULL CONSTRAINT sr_csId_fk FOREIGN KEY(customerId) REFERENCES customer(customerId),
+	mechanicId INT NOT NULL CONSTRAINT sr_mcId_fk FOREIGN KEY(mechanicId) REFERENCES mechanic(mechanicId)
 );
---One service record could contain many services
---One record could contain many vehicles
-
-CREATE TABLE mechanic
-(
-	mechanicId INT PRIMARY KEY IDENTITY(1,1),	--Auto incrementing
-	firstName NVARCHAR(25) NOT NULL,
-	lastName NVARCHAR(25) NOT NULL,
-	salary SMALLMONEY NOT NULL,
-	contactNumber VARCHAR(25) NOT NULL, --Catering for international long numbers
-	email NVARCHAR(40) NOT NULL,
-	serviceId INT NOT NULL CONSTRAINT mh_sId_fk FOREIGN KEY(serviceId) REFERENCES [service](serviceId),
-	recordId INT NOT NULL CONSTRAINT mh_srId_fk FOREIGN KEY(recordId) REFERENCES serviceRecord(recordId)
-);
---One mechanic could contain many services
+--One service could contain many service records
+--One customer could contain many service records
 --One mechanic could contain many service records
-
-CREATE TABLE customer
-(
-	customerId INT PRIMARY KEY IDENTITY(1,1),
-	firstName NVARCHAR(25) NOT NULL,
-	lastName NVARCHAR(25) NOT NULL,
-	contactNumber VARCHAR(25) NOT NULL,
-	email NVARCHAR(40) NOT NULL,
-	[address] NVARCHAR(80) NOT NULL,
-	mechanicId INT NOT NULL CONSTRAINT cu_mId_fk FOREIGN KEY(mechanicId) REFERENCES mechanic(mechanicId),
-	recordId INT NOT NULL CONSTRAINT ct_srId_fk FOREIGN KEY(recordId) REFERENCES serviceRecord(recordId)
-);
---One customer could contain many mechanics
---One customer could contain many serviceRecords
-
-CREATE TABLE vehicle
-(
-	vehicleId INT PRIMARY KEY IDENTITY(1,1),
-	brand NVARCHAR(30) NOT NULL,
-	model NVARCHAR(30) NOT NULL,
-	[year] DATE NOT NULL,
-	plate NVARCHAR(30) NOT NULL,	--Also catering for international and custom number plates
-	customerId INT NOT NULL CONSTRAINT vh_cId_fk FOREIGN KEY(customerId) REFERENCES customer(customerId),
-);
---One customer could contain many vehicles
-
 
 
 
