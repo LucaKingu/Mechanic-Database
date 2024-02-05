9760351b79357bc67fed1b0bdf89ece8d65a1016
Luca Alfino <alfinoluca21@gmail.com>
2024-02-05 19:30:46 +0100

    Added close connection button and finding best way to design the application

--- MechanicBackend/MechanicBackend/Form2.cs
+++ MechanicBackend/MechanicBackend/Form2.cs
@@ -1,44 +1,34 @@
-using System.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace MechanicBackend
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        private SqlConnection conn;
+
+        public Form2(SqlConnection conn)
         {
             InitializeComponent();
-        }
+            this.conn = conn;   //Connect with Form1 connection
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
+            if (this.conn.State == System.Data.ConnectionState.Closed)
+            {
+                this.conn.Open();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string connectionString;
-            SqlConnection conn;
-
-            connectionString = @"Data Source=DESKTOP-I830V2D; Initial Catalog=MechanicDB;Integrated Security=True;";
-            //Integrated security = True; So that you can easily connect to the database engine via windows authentication on your local machine
-            conn = new SqlConnection(connectionString);
-
-            conn.Open();
-            MessageBox.Show("Connection Succesfully open!");
-
-            //The below is testing for queries to see the data is indeed being extracted
-
-            /* SqlCommand command;
+            SqlCommand command;
             SqlDataReader reader;
             string query = "";
             string output = "";
@@ -50,7 +40,7 @@ namespace MechanicBackend
             reader = command.ExecuteReader();
 
             //Access each row one by one
-            while(reader.Read())
+            while (reader.Read())
             {
                 output = output + reader.GetValue(0) + " - " + reader.GetValue(1) + " - "
                                 + reader.GetValue(2) + " - " + reader.GetValue(3) + " - "
@@ -60,12 +50,16 @@ namespace MechanicBackend
             MessageBox.Show(output);
 
             reader.Close();
-            command.Dispose(); */
+            command.Dispose();
+        }
 
-            Form2 form2 = new Form2(conn);
-            form2.Show();
-
-            //conn.Close();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(this.conn.State == System.Data.ConnectionState.Open)
+            {
+                this.conn.Close();
+                MessageBox.Show("Connection Closed");
+            }
         }
     }
 }
