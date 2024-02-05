01807d1785c9e505bfab93aaafb76a681a1bb3c1
Luca Alfino <alfinoluca21@gmail.com>
2024-02-05 18:39:57 +0100

    Data extraction test success

--- MechanicBackend/MechanicBackend/Form1.cs
+++ MechanicBackend/MechanicBackend/Form1.cs
@@ -30,11 +30,37 @@ namespace MechanicBackend
             SqlConnection conn;
 
             connectionString = @"Data Source=DESKTOP-I830V2D; Initial Catalog=MechanicDB;Integrated Security=True;";
+            //Integrated security = True; So that you can easily connect to the database engine via windows authentication on your local machine
             conn = new SqlConnection(connectionString);
 
             conn.Open();
             MessageBox.Show("Connection Succesfully open!");
 
+            //The below is testing for queries to see the data is indeed being extracted
+
+            /* SqlCommand command;
+            SqlDataReader reader;
+            string query = "";
+            string output = "";
+
+            query = "SELECT * FROM dbo.customer";
+
+            command = new SqlCommand(query, conn);
+
+            reader = command.ExecuteReader();
+
+            //Access each row one by one
+            while(reader.Read())
+            {
+                output = output + reader.GetValue(0) + " - " + reader.GetValue(1) + " - "
+                                + reader.GetValue(2) + " - " + reader.GetValue(3) + " - "
+                                + reader.GetValue(4) + " - " + reader.GetValue(5) + " - ";
+            }
+
+            MessageBox.Show(output);
+
+            reader.Close();
+            command.Dispose(); */
             conn.Close();
         }
     }
